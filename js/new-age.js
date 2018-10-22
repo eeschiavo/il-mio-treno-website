4bffcf5270fe77b5d7e310a4fd53f9765e5ccb91
Ernesto Schiavo <schiavo.ernesto@gmail.com>
2018-10-22 21:43:39 +0200

    Aggiunta sezione features e sezione download

--- js/new-age.js
+++ js/new-age.js
@@ -1,8 +1,36 @@
 (function($) {
     "use strict"; // Start of use strict
 
+    function getWindowRelativeOffset(parentWindow, elem) {
+            var offset = {
+                left : 0,
+                top : 0
+            };
+            // relative to the target field's document
+            offset.left = elem.getBoundingClientRect().left;
+            offset.top = elem.getBoundingClientRect().top;
+            // now we will calculate according to the current document, this current
+            // document might be same as the document of target field or it may be
+            // parent of the document of the target field
+            var childWindow = elem.document.frames.window;
+            while (childWindow != parentWindow) {
+                offset.left = offset.left + childWindow.frameElement.getBoundingClientRect().left;
+                offset.top = offset.top + childWindow.frameElement.getBoundingClientRect().top;
+                childWindow = childWindow.parent;
+            }
+            return offset;
+        };
+
+    window.animateItem = function(id) {
+        console.log($(id).offset().top);
+        console.log($(id).offset().top - $(window).scrollTop());
+        $('html, body').animate({
+            scrollTop: ($(id).offset().top - 48)
+        }, 1000, "easeInOutExpo");
+    }
+
     // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    $('a.js-scroll-trigger[href*="#"]').click(function() {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -82,7 +110,5 @@
 
           ]
     });
-
-
 
 })(jQuery); // End of use strict
